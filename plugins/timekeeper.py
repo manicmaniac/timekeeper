1a3cde941fb7b17af68f03bce87d3dcc05a6d7c5
Ryosuke Ito <rito.0305@gmail.com>
2016-09-28 01:03:36 +0900

    fix an error occurs when upload file to private channel

--- plugins/timekeeper.py
+++ plugins/timekeeper.py
@@ -201,7 +201,7 @@ def show_timesheet(message, user):
     timesheet = render_timesheet(attendances)
     filename = 'timesheet.md'
     with create_tmp_file(bytes(timesheet, 'utf-8')) as path:
-        message.channel.upload_file(filename, path, 'Here is your timesheet.'.format(user.timezone_id))
+        safe_upload_file(message, filename, path, 'Here is your timesheet.', is_text_file=True)
 
 
 @respond_to('contributions')
@@ -215,7 +215,7 @@ def show_contributions(message, user):
         filename = 'contributions.png'
         path = os.path.join(tempdir, filename)
         save_contributions_image(path, user.attendances)
-        message.channel.upload_file(filename, path, 'Here. Regardless of your timezone, each days are plotted in UTC.')
+        safe_upload_file(message, filename, path, 'Here. Regardless of your timezone, each days are plotted in UTC.')
     finally:
         rmtree(tempdir)
 
@@ -229,7 +229,26 @@ def debug(message, script):
 @respond_to('quine')
 def quine(message):
     with open(__file__, 'r') as f:
-        message.channel.upload_file(f.name, f.name, '')
+        safe_upload_file(message, f.name, f.name, '', is_text_file=True)
+
+
+def safe_upload_file(message, filename, path, comment, is_text_file=False):
+    channel_id = message.body['channel']
+    if not is_direct_message(channel_id):
+        return message.channel.upload_file(filename, path, comment)
+    if not is_text_file:
+        return message.reply('Sorry, I cannot upload a file in the private channel.')
+    with open(path) as f:
+        text = f.read()
+        message.reply(triple_backquoted(text))
+
+
+def triple_backquoted(text):
+    return '```\n' + text + '\n```'
+
+
+def is_direct_message(channel_id):
+    return channel_id.startswith('D')
 
 
 def render_timesheet(attendances):
