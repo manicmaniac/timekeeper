8b27640ace9a9a93aa8983aec9847af52a58c302
ymizushima <yodaimizushima@gmail.com>
2016-10-04 02:10:10 +0900

    reduce code in try-except block

--- bot.py
+++ bot.py
@@ -9,14 +9,15 @@ lock_socket = None
 def is_lock_free():
     global lock_socket
     lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    lock_id = 'timekeeper-slackbot'
     try:
-        lock_id = 'timekeeper-slackbot'
         lock_socket.bind('\0' + lock_id)
-        logging.debug('Acquired lock {!r}'.format(lock_id))
-        return True
     except socket.error:
         logging.info('Failed to acquire lock {!r}'.format(lock_id))
         return False
+    else:
+        logging.debug('Acquired lock {!r}'.format(lock_id))
+        return True
 
 
 def main():
