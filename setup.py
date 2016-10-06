1b45f046b41913d2ea3f806e5dc8dcc2270eda26
ymizushima <yodaimizushima@gmail.com>
2016-10-07 00:48:06 +0900

    add missing packages

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='timekeeper',
     description='A nosy Slack bot to track your daily workload.',
     long_description=open('README.rst').read(),
-    version='0.0.2',
+    version='0.0.3',
     py_modules=['bot', 'slackbot_settings'],
-    packages=['timekeeper'],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=open('requirements.txt').read(),
+    test_suite='tests'
 )
