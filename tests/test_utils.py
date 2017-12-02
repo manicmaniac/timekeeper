8d19158b88c17c3f229329ae354a771822f5bcfe
Ryosuke Ito <rito.0305@gmail.com>
2017-12-03 02:45:06 +0900

    add model tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,15 +5,17 @@ from timekeeper.utils import format_timedelta
 
 
 class TestUtils(unittest.TestCase):
-    def test_format_timedelta(self):
-        result = format_timedelta(timedelta(seconds=36))
-        self.assertEqual('00:00:36', result)
-
-        result = format_timedelta(timedelta(seconds=360))
-        self.assertEqual('00:06:00', result)
-
-        result = format_timedelta(timedelta(seconds=3600))
-        self.assertEqual('01:00:00', result)
-
-        result = format_timedelta(timedelta(seconds=36000))
-        self.assertEqual('10:00:00', result)
+    def test_format_timedelta_parameterized(self):
+        # (seconds, expected_value)
+        parameters = [
+            (36, '00:00:36'),
+            (360, '00:06:00'),
+            (3600, '01:00:00'),
+            (36000, '10:00:00'),
+            (24 * 60 * 60, '24:00:00'),
+            (359999, '99:59:59'),
+            (360000, '100:00:00'),
+        ]
+        for seconds, expected_value in parameters:
+            actual_value = format_timedelta(timedelta(seconds=seconds))
+            self.assertEqual(actual_value, expected_value)
